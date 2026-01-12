4c95fc2b6e1232eb68e1223abeb5c7080f47c853
akasha717 <suleimanakasha717@gmail.com>
2026-01-12 22:36:31 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   } = supabase.auth.onAuthStateChange((_event, session) => {
     setUser(session?.user ?? null);
   });
-      export default function Home() {
+      
   const [session, setSession] = useState<any>(null)
 
   useEffect(() => {
@@ -111,6 +111,7 @@ export default function Home() {
     </main>
   );
 }
+
 
 
 
