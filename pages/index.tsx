2812b6f488abea8675628a300d5b5995e1b916a2
akasha717 <suleimanakasha717@gmail.com>
2026-01-14 15:47:37 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -135,7 +135,7 @@ export default function Home() {
     supabase.auth.signUp({ email, password })
   const signOut = async () => supabase.auth.signOut()
 return (
-  <div>
+  <>
     <main className="page">
       <style jsx global>{`
   body {
@@ -449,6 +449,7 @@ return (
     </main>
   )
 }
+
 
 
 
